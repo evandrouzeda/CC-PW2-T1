6a51147c44a131bba557dc522e2a2893dba0158a
evandrouzeda <evandrouzeda@gmail.com>
2021-12-10 12:04:45 -0300

    arrumando alert

--- src/componentes/Alert.jsx
+++ src/componentes/Alert.jsx
@@ -1,19 +1,12 @@
 import { Component } from "react";
 
 export default class Alert extends Component {
-    state = { msg: "teste" }
     mostraAlert(status) {
         console.log(`${status.status} - ${status.msg}`);
         switch (status.status) {
             case "success":
-                /**
-                 * Nao entendi pq, mas quando uso o setState 
-                 * para mudar o msg ele entra num loop infinito
-                 */
-                //this.setState({ msg: status.msg })
                 return "alert alert-success"
             case "error":
-                //this.setState({ msg: status.msg })
                 return "alert alert-danger"
             default:
                 return "d-none"
@@ -22,7 +15,7 @@ export default class Alert extends Component {
     render() {
         return (
             <div className={this.mostraAlert(this.props.status)} role="alert">
-                {this.state.msg}
+                {this.props.status.msg}
             </div>
         )
     }
